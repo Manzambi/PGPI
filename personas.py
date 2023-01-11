6b7ce65ed5cee4c651d95453b910c4da8dda59cb
Manzambi Antonio K. Doge <manzambigomez@correo.ugr.es>
2023-01-11 14:26:11 +0100

    estamosactualizar la clase persona

--- personas.py
+++ personas.py
@@ -1,7 +1,15 @@
 nombre = "lucas morais"
-edad= "27"
+edad= 27
+morada="calle del beiro"
+nacionalidad = "española"
+cidad ="granada"
+codigoPostal = 18012
+
 def persona():
     print(nombre)
     print(edad)
-
+    print(morada)
+    print(nacionalidad)
+    print(cidad)
+    print(codigoPostal)
 persona()
